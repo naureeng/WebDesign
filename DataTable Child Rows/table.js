e3955ddf12b938d3bc3fec821087c9e5102be01f
Naureen Ghani <32179063+naureeng@users.noreply.github.com>
2018-03-26 10:21:45 -0400

    Add files via upload

---
+++ DataTable Child Rows/table.js
@@ -0,0 +1,9 @@
+/* Formatting function for row details, modify as you need */
+
+function format (d) {
+  // 'd' is the original data object for the row
+  return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+  '<tr>' +
+    '<td> Full Name: </td>' +
+    
+}
